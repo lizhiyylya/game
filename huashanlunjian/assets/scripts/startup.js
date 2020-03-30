871cc1c6d8822a5901550422c096d88d596d628e
liz-i <liz-i@glodon.com>
2020-03-30 18:00:25 +0800

    add my game

---
+++ huashanlunjian/assets/scripts/startup.js
@@ -0,0 +1,38 @@
+// Learn cc.Class:
+//  - https://docs.cocos.com/creator/manual/en/scripting/class.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        bcPrefab: {
+            default: null,
+            type: cc.Prefab
+        }
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {},
+    onLoad: function () {
+        this.spawnNewStar();
+    },
+
+    spawnNewStar: function() {
+        // 使用给定的模板在场景中生成一个新节点
+        var newStar = cc.instantiate(this.bcPrefab);
+        // 将新增的节点添加到 Canvas 节点下面
+        this.node.addChild(newStar);
+        // 为星星设置一个随机位置
+    },
+
+    start () {
+
+    },
+
+    // update (dt) {},
+});
